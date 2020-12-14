dbc58d63b4921e38203ad9afa9f2d3962ad04aaa
Jin Choo <jinchoo315@gmail.com>
2020-12-14 00:16:51 -0600

    Testing Heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var methodOverride = require("method-override");
+// var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 3800;
 
@@ -8,11 +8,11 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.json({ type: "application/*+json" }));
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html " }));
+// app.use(bodyParser.json({ type: "application/*+json" }));
+// app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+// app.use(bodyParser.text({ type: "text/html " }));
 
-app.use(methodOverride("_method"));
+// app.use(methodOverride("_method"));
 
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
