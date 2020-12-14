741082135783ad3f11d5f5dcbdf5446eeb68f869
Jin Choo <jinchoo315@gmail.com>
2020-12-13 23:42:13 -0600

    Added README.md and still working on one of the buttons

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-var PORT = process.env.PORT || 3050;
+var PORT = process.env.PORT || 3800;
 
 var app = express();
 app.use(express.static("public"));
