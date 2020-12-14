9aca49b750734682d01278707f7aa65b1b343bc9
Jin Choo <jinchoo315@gmail.com>
2020-12-13 18:17:16 -0600

    still having same issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-var PORT = process.env.PORT || 4000;
+var PORT = process.env.PORT || 5050;
 
 var app = express();
 app.use(express.static("public"));
@@ -15,7 +15,7 @@ app.use(bodyParser.text({ type: "text/html " }));
 app.use(methodOverride("_method"));
 
 var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defalutLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller.js");
