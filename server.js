9ba3eb3429e80a5a18ff266f4218ecbcd183c043
Jin Choo <jinchoo315@gmail.com>
2020-12-14 16:12:03 -0600

    Initial push to heroku; dynamic app and db connection

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 // var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-var PORT = process.env.PORT || 3800;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 app.use(express.static("public"));
